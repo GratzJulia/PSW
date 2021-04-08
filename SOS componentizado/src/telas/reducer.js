c1ceb4cc9454f211adc8594e70f4b2a1b73f7f8a
GratzJulia <julia.gratz@aluno.cefet-rj.br>
2021-04-08 19:41:57 -0300

    RootReducer e Store
    Finalizacao do reducer e da store. Servem para agregar os dados da aplicacao num local unico.

--- SOS componentizado/src/telas/reducer.js
+++ SOS componentizado/src/telas/reducer.js
@@ -1,14 +1,34 @@
 import { createStore } from "redux";
-import mockSintoma from "../mocks/mockSintoma";
+import TESTE from "../mocks/TESTE";
 
-const estadoInicial = mockSintoma;
-
-function sintoma(state = estadoInicial, action) {
+function reducerTree(state = TESTE, action) {
   switch (action.type) {
+    case "SINTOMA_ADD":
+      state.Sintomas.push(action.newS);
+      return state.Sintomas;
+    case "ATENDIMENTO_ADD":
+      state.Atendimentos.push(action.newAt);
+      return state.Atendimentos;
+    case "ACIDENTE_ADD":
+      state.Acidentes.push(action.newAc);
+      return state.Acidentes;
+    case "PTO_ADD":
+      state.PontosAtendimento.push(action.newPT);
+      return state.PontosAtendimento;
     default:
       return state;
   }
 }
 
-const store = createStore(sintoma);
+const store = createStore(reducerTree);
+// store.dispatch({
+//   type: "SINTOMA_ADD",
+//   newS: {
+//     data: "datetime",
+//     nome: "cólica",
+//     descricao: "durou a tarde toda"
+//   }
+// });
+
+//console.log(store.getState());
 export default store;
